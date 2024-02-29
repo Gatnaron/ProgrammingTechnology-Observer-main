764f6b1c1a960684a178fb9eef2f715844db70ec
Gatnaron <oganyan.ivan2016@yandex.ru>
2024-02-29 23:21:53 +0300

    update ClockComponent - localTime

--- src/main/java/ClockComponents/ClockComponent.java
+++ src/main/java/ClockComponents/ClockComponent.java
@@ -1,18 +1,20 @@
 package ClockComponents;
 
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import observerComponents.Infastructure.Observer;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class ClockComponent implements Observer {
     private final Text text = new Text();
+    LocalTime localTime = LocalTime.of(0, 0, 0);
     @Override
     public void update() {
-        var dateFormat = new SimpleDateFormat("HH:mm:ss");
-        var currentTime = dateFormat.format(new Date());
-        text.setText(currentTime);
+        localTime = localTime.plusSeconds(1);
+        text.setText(String.valueOf(localTime.getHour())+":"+String.valueOf(localTime.getMinute())+":"+String.valueOf(localTime.getSecond()));
     }
 
     public Text getClock(){
